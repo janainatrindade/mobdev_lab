288a78ba013991bdbb0c83b672b0d466bb5bc6ca
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-03 23:43:40 +0000

    chnage views

--- mobdev-ionic-intro/src/app/pages/people/people.page.ts
+++ mobdev-ionic-intro/src/app/pages/people/people.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-people',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeoplePage implements OnInit {
 
-  constructor() { }
+  constructor(private navController: NavController, private router: Router) { }
 
   ngOnInit() {
   }
+
+  openDetails(){
+    this.router.navigateByUrl(`/tabs/films/42`);
+    }
+  
+    goToPlanets(){
+      this.navController.navigateRoot('/tabs/planets')
+    }
 
 }
--- mobdev-ionic-intro/src/app/pages/planets/planets.page.ts
+++ mobdev-ionic-intro/src/app/pages/planets/planets.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-planets',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetsPage implements OnInit {
 
-  constructor() { }
+  constructor(private navController: NavController, private router: Router) { }
+
 
   ngOnInit() {
   }
+
+  openDetails(){
+    this.router.navigateByUrl(`/tabs/films/42`);
+    }
+  
+    goToPlanets(){
+      this.navController.navigateRoot('/tabs/planets')
+    }
 
 }
