818fb243664bcf2754f99010ea748ab63c4cb3ce
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-03 22:34:30 +0000

    tab bar

--- mobdev-ionic-intro/src/app/app-routing.module.ts
+++ mobdev-ionic-intro/src/app/app-routing.module.ts
@@ -3,31 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
-  },
-  {
-    path: 'people',
-    loadChildren: () => import('./pages/people/people.module').then( m => m.PeoplePageModule)
-  },
-  {
-    path: 'planets',
-    loadChildren: () => import('./pages/planets/planets.module').then( m => m.PlanetsPageModule)
-  },
-  {
-    path: 'films',
-    loadChildren: () => import('./pages/films/films.module').then( m => m.FilmsPageModule)
-  },
-  {
-    path: 'film-details',
-    loadChildren: () => import('./pages/film-details/film-details.module').then( m => m.FilmDetailsPageModule)
-  },
+  {path: '',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
 
 @NgModule({
