48f1acd54edbead17dd11371db443e843cd7414f
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-04 00:39:35 +0000

    updates

--- mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ApiService } from './../../services/api.service';
-
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-film-details',
   templateUrl: './film-details.page.html',
   styleUrls: ['./film-details.page.scss'],
 })
 export class FilmDetailsPage implements OnInit {
-
+ 
   film: any;
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
-  
+ 
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+ 
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.api.getFilm(id).subscribe(res => {
+    this.http.get(`https://swapi.dev/api/films/${id}`).subscribe(res => {
       this.film = res;
     });
   }
-
+ 
 }
