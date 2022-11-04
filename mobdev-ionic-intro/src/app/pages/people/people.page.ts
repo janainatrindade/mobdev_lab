9d86be078bd26aa011b8a15d591c8cd928902856
Janaina Trindade <janaina_tm_sap@hotmail.com>
2022-11-04 14:05:52 +0000

    people page

--- mobdev-ionic-intro/src/app/pages/people/people.page.ts
+++ mobdev-ionic-intro/src/app/pages/people/people.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-people',
@@ -9,17 +11,18 @@ import { NavController } from '@ionic/angular';
 })
 export class PeoplePage implements OnInit {
 
-  constructor(private navController: NavController, private router: Router) { }
+  people: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.people = this.http.get('https://swapi.dev/api/people');
   }
-
-  openDetails(){
-    this.router.navigateByUrl(`/tabs/films/42`);
-    }
-  
-    goToPlanets(){
-      this.navController.navigateRoot('/tabs/planets')
-    }
+ 
+  openDetails(people) {
+    let split = people.url.split('/');
+    let peopleID = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/people/${peopleID}`);
+  }
 
 }
